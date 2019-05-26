3d839a37e1875c25af0989b4690caa24e7b5d514
Sinma <damianciftci@gmail.com>
2019-05-26 14:49:55 +0200

    Reverted BaseModel

--- src/main/java/com/library/Models/BaseModel.java
+++ src/main/java/com/library/Models/BaseModel.java
@@ -3,7 +3,5 @@ package com.library.Models;
 import java.sql.SQLException;
 
 public abstract class BaseModel {
-    public abstract static class db {
-        public abstract void save() throws SQLException;
-    }
+    public abstract void save() throws SQLException;
 }
